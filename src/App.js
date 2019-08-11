e483a8bc7191422fb6c36a96d5dd5946e0388ffb
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-08-11 09:10:12 -0700

    initial push starting to install and wire dependencies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { render } from "react-dom";
+import lasVegas from './img/vegas.jpg';
+import './css/index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App-container">
+        <header className="App-header">
+       
+        
+    
+        </header>
+        <div className="name-card-container">
+          {/* <img src={lasVegas} className="las-vegas" alt="photo of las vegas" /> */}
+          <h2 className="h2-fname">Jeremy</h2><h2 className="h2-lname">Brady</h2>
+          <h3>Full Stack Web Developer</h3>
+          <h3>& Software Engineer</h3>
+        </div>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
